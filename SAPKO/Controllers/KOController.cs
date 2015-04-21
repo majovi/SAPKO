b17b0539d5400360c37b40bf96699267832de396
majovi <majovi@majovi.dk>
2015-04-21 14:42:12 +0200

    KO added

---
+++ SAPKO/Controllers/KOController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAPKO.Controllers
+{
+    public class KOController : Controller
+    {
+        // GET: KO
+        public ActionResult KO()
+        {
+            return View();
+        }
+    }
+}
