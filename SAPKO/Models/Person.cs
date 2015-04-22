d1f76b70a62b182b68a3fa05ba585fb8fbc4f5ce
Martin Larsen <majovi@majovi.dk>
2015-04-22 14:41:50 +0200

    Person Model

---
+++ SAPKO/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAPKO.Models
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
